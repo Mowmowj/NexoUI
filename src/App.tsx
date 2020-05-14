80df1465cafc489d3d792b463060f8b87a003bfb
Mowmowj <274334790@qq.com>
2020-05-15 02:01:33 +0800

    button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button,{ ButtonType, ButtonSize } from './components/Button/button';
 
-function App() {
+const App:React.FC=( )=> {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>Normal Vamonos</Button>
+        <Button disabled>Disabled Vamonos</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Vamonos </Button>
+        <Button btnType={ButtonType.Link} href="https://www.google.com"> Link Vamonos </Button>
+        <Button btnType={ButtonType.Link} href="https://www.google.com" disabled> Disabled Link Vamonos </Button>
       </header>
     </div>
   );
